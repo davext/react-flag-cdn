45043cc55e2fa9e4c3b6f4708fd197b1b7756d9b
David Harvey <dave_xt@hotmail.com>
2022-01-27 12:30:42 -0800

    feature: specifying type

--- src/Flag.js
+++ src/Flag.js
@@ -1,17 +1,15 @@
 import React from "react";
-import PropTypes from 'react-proptypes';
+import PropTypes from 'prop-types';
 import {countryISOs} from "./countryISOs";
 
 /**
  * Renders a <Flag /> component
  * @param  props
- * @param  props.width - The size of the flag
- * @param  props.textColor - the color of the text in the button
- * @param  props.bgColor - the background color of the button
- * @param  props.overrideStyles - used to set the CSS of the button
+ * @param  {number} props.width - The size of the flag
+ * @param  {string} props.country - the color of the text in the button
+ * @param  {string} props.type - type of the image returned. ['svg', 'jpg', 'png'] defaults to svg.
  */
 const Flag = (props) =>{
-
     return (
         <img
             {...props}
@@ -25,6 +23,8 @@ Flag.propTypes = {
    * Country code in ISO 3166 format. See https://www.iso.org/iso-3166-country-codes.html
    */
   country: PropTypes.oneOf(Object.keys(countryISOs)),
+  width: PropTypes.number,
+  type: PropTypes.oneOf(["svg","png","jpg"])
 }
 
 
